256b9f408872ae2f5c7378812ac2555bfaa413cd
4id3n063 <141164903+4id3n063@users.noreply.github.com>
2026-02-04 12:32:58 -0600

    ok

--- public/netflix.js
+++ public/netflix.js
@@ -50,7 +50,7 @@ address.addEventListener("click", async (event) => {
 		throw err;
 	}
 
-	const url = search("https://www.xbox.com/en-us/play");
+	const url = search("netflix.com");
 
 	const wispUrl =
 		(location.protocol === "https:" ? "wss" : "ws") +
